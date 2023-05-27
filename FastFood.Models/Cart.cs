7d3a05ac1361faf541f31cc29ff52882fcdbd6eb
Mirza Mujtaba <Mirza Mujtaba@DESKTOP-14420OK>
2023-05-27 13:54:59 +0500

    Add project files.

---
+++ FastFood.Models/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+         ApplicationUser ApplicationUser {get; set;}
+        public Item Item { get; set; }
+        public string AplicationUserId { get; set; }
+        [Required,MinLength(1)]
+        public int Count { get; set; }
+
+
+
+    }
+}
---
+++ FastFood.Repository/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastFood.Models;
+
+namespace FastFood.Repository
+{
+    internal class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+
+        public DbSet<OrderHeader> OrderHeader { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+
+    }
+}
